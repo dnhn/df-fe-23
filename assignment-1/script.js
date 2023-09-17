3d56bdbcb3e2a4e909d957ac8c7ce5f2edaa0179
Nhan Doan <2561973+dnhn@users.noreply.github.com>
2023-09-18 00:17:49 +0700

    Delete row

--- assignment-1/script.js
+++ assignment-1/script.js
@@ -1,1 +1,16 @@
-// Your JS code goes here
+document.querySelectorAll('table .row')
+  .forEach(row => {
+    row.querySelector('.row__action').addEventListener('click', () => {
+      row.classList.add('row--confirm');
+    });
+
+    row.querySelector('.row__confirm').addEventListener('click', () => {
+      if (row.classList.contains('book-row')) {
+        row.remove();
+      }
+    });
+
+    row.querySelector('.row__cancel').addEventListener('click', () => {
+      row.classList.remove('row--confirm');
+    });
+  });
