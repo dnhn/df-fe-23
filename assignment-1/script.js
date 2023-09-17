5c6012a56372029bd853f6b38b5138e94b19b371
Nhan Doan <2561973+dnhn@users.noreply.github.com>
2023-09-18 01:17:26 +0700

    Add book

--- assignment-1/script.js
+++ assignment-1/script.js
@@ -1,16 +1,50 @@
-document.querySelectorAll('table .row')
-  .forEach(row => {
-    row.querySelector('.row__action').addEventListener('click', () => {
-      row.classList.add('row--confirm');
-    });
+const addRow = document.querySelector('.add-row');
+const addBookForm = document.getElementById('add-book-form');
+const bookTemplate = (title, author, topic) => `<tr class="row book-row">
+  <td>${title}</td>
+  <td>${author}</td>
+  <td>${topic}</td>
+  <td class="row__actions">
+    <button type="button" class="row__action btn btn--warning">Delete</button>
+    <div class="row__confirm">
+      <button type="button" class="row__confirm btn">Confirm</button>
+      <button type="button" class="row__cancel btn btn--error">Cancel</button>
+    </div>
+  </td>
+</tr>`;
 
-    row.querySelector('.row__confirm').addEventListener('click', () => {
-      if (row.classList.contains('book-row')) {
-        row.remove();
-      }
-    });
+function addDeleteEventListener(element) {
+  element.querySelector('.row__action').addEventListener('click', () => {
+    element.classList.add('row--confirm');
 
-    row.querySelector('.row__cancel').addEventListener('click', () => {
-      row.classList.remove('row--confirm');
-    });
+    if (element.classList.contains('add-row')) {
+      addBookForm[0].focus();
+    }
   });
+
+  element.querySelector('.row__confirm').addEventListener('click', () => {
+    if (element.classList.contains('book-row')) {
+      element.remove();
+    }
+  });
+
+  element.querySelector('.row__cancel').addEventListener('click', () => {
+    element.classList.remove('row--confirm');
+  });
+}
+
+document.querySelectorAll('.book-list .row')
+  .forEach(row => addDeleteEventListener(row));
+
+addBookForm.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const form = event.target;
+  const book = bookTemplate(form[0].value, form[1].value, topics[form[2].value]);
+
+  document.querySelector('.book-list tbody').insertAdjacentHTML('beforeend', book);
+  addDeleteEventListener(document.querySelector('.book-list tbody tr:last-child'));
+
+  form.reset();
+  addRow.classList.remove('row--confirm');
+});
