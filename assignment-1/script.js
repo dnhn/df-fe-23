e9d5c82682217b359e4eab039d0a35637e66fd81
Nhan Doan <2561973+dnhn@users.noreply.github.com>
2023-09-18 03:07:11 +0700

    Render list

--- assignment-1/script.js
+++ assignment-1/script.js
@@ -5,10 +5,38 @@ const topics = {
   programming: 'Programming',
   vcs: 'Version control',
 };
+let books = [
+  {
+    id: Date.now(),
+    title: 'HTML & CSS 101',
+    author: 'W3C',
+    topic: 'Front-end',
+  },
+  {
+    id: Date.now() + 1000,
+    title: 'Responsive Web Design',
+    author: 'Ethan Marcotte',
+    topic: 'Front-end',
+  },
+  {
+    id: Date.now() + 2000,
+    title: 'JavaScript Patterns',
+    author: 'Addy Osmani',
+    topic: 'Programming',
+  },
+  {
+    id: Date.now() + 3000,
+    title: 'Git',
+    author: 'Linus Torvalds',
+    topic: 'Version Control',
+  },
+];
 
+const search = document.getElementById('search');
 const addRow = document.querySelector('.add-row');
 const addBookForm = document.getElementById('add-book-form');
-const bookTemplate = (title, author, topic) => `<tr class="row book-row">
+const bookTemplate = ({ id, title, author, topic }) =>
+  `<tr class="row book-row" data-id="${id}">
   <td>${title}</td>
   <td>${author}</td>
   <td>${topic}</td>
@@ -41,23 +69,42 @@ function addDeleteEventListener(element) {
   });
 }
 
+function renderList() {
+  let html = '';
+
+  for (let i = 0; i < books.length; i++) {
+    html += bookTemplate(books[i]);
+  }
+
+  if (html.length) {
+    document.querySelector('.book-list tbody').textContent = '';
+    document.querySelector('.book-list tbody').insertAdjacentHTML('beforeend', html);
+    document.querySelectorAll('.book-list .row').forEach(row => addDeleteEventListener(row));
+  }
+}
+
 document.querySelector('.table-header button').addEventListener('click', () => {
   addRow.classList.toggle('row--confirm');
   addBookForm[0].focus();
 });
 
-document.querySelectorAll('.book-list .row')
-  .forEach(row => addDeleteEventListener(row));
-
 addBookForm.addEventListener('submit', event => {
   event.preventDefault();
 
   const form = event.target;
-  const book = bookTemplate(form[0].value, form[1].value, topics[form[2].value]);
+  const book = {
+    id: Date.now(),
+    title: form[0].value,
+    author: form[1].value,
+    topic: topics[form[2].value],
+  };
 
-  document.querySelector('.book-list tbody').insertAdjacentHTML('beforeend', book);
+  books.push(book);
+  document.querySelector('.book-list tbody').insertAdjacentHTML('beforeend', bookTemplate(book));
   addDeleteEventListener(document.querySelector('.book-list tbody tr:last-child'));
 
   form.reset();
   addRow.classList.remove('row--confirm');
 });
+
+renderList();
