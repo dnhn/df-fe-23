1cba5c2b985d20720c00b6eb61348c7ec0a22ed3
Nhan Doan <2561973+dnhn@users.noreply.github.com>
2023-09-24 23:01:13 +0700

    Add data

--- assignment-2/src/common/data.js
+++ assignment-2/src/common/data.js
@@ -1,7 +1,9 @@
 export const BOOK_TOPICS = {
   backend: 'Back-end',
+  design: 'Design',
   frontend: 'Front-end',
   fullstack: 'Full stack',
+  ml: 'Machine Learning',
   programming: 'Programming',
   vcs: 'Version control',
 };
@@ -30,5 +32,83 @@ export const INITIAL_BOOKS = [
     title: 'Git',
     author: 'Linus Torvalds',
     topic: 'vcs',
+  },
+  {
+    id: '4',
+    title: 'Smart Interface Design Patterns',
+    author: 'Vitaly Friedman',
+    topic: 'design',
+  },
+  {
+    id: '5',
+    title: 'The Smashing Book',
+    author: 'Smashing Magazine',
+    topic: 'design',
+  },
+  {
+    id: '6',
+    title: 'Node.js',
+    author: 'Ryan Dahl',
+    topic: 'backend',
+  },
+  {
+    id: '7',
+    title: 'Designing Machine Learning Systems',
+    author: 'Chip Huyen',
+    topic: 'ml',
+  },
+  {
+    id: '8',
+    title: 'CSS Tricks',
+    author: 'Chris Coyier',
+    topic: 'frontend',
+  },
+  {
+    id: '9',
+    title: 'HTML Semantics',
+    author: 'Smashing Magazine',
+    topic: 'frontend',
+  },
+  {
+    id: '10',
+    title: 'JavaScript: The Definitive Guide',
+    author: 'David Flanagan',
+    topic: 'programming',
+  },
+  {
+    id: '11',
+    title: 'Atomic Design',
+    author: 'Brad Frost',
+    topic: 'design',
+  },
+  {
+    id: '12',
+    title: 'TypeScript in 50 Lessons',
+    author: 'Stefan Baumgartner',
+    topic: 'programming',
+  },
+  {
+    id: '13',
+    title: 'Software Engineering - The Soft Parts',
+    author: 'Addy Osmani',
+    topic: 'programming',
+  },
+  {
+    id: '14',
+    title: 'Image Optimization',
+    author: 'Addy Osmani',
+    topic: 'frontend',
+  },
+  {
+    id: '15',
+    title: 'jQuery',
+    author: 'John Resig',
+    topic: 'frontend',
+  },
+  {
+    id: '16',
+    title: 'Designing Data-Intensive Applications',
+    author: 'Martin Kleppmann',
+    topic: 'programming',
   },
 ];
