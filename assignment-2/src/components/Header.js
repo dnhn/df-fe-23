de4c2b9b4f6f8cce342b8a531c7d6f68df61cca4
Nhan Doan <2561973+dnhn@users.noreply.github.com>
2023-09-25 15:16:04 +0700

    Theme switch

--- assignment-2/src/components/Header.js
+++ assignment-2/src/components/Header.js
@@ -1,3 +1,5 @@
+import ThemeSwitch from './ThemeSwitch';
+
 import './Header.css';
 
 export default function Header() {
@@ -6,7 +8,10 @@ export default function Header() {
       <a href="/" className="logo">
         <h1>Bookstore</h1>
       </a>
-      <a href="https://github.com/dnhn/df-fe-23/tree/main/assignment-2" target="_blank" rel="noreferrer noopener" className="user">GitHub</a>
+      <div className="header__right">
+        <ThemeSwitch />
+        <a href="https://github.com/dnhn/df-fe-23/tree/main/assignment-2" target="_blank" rel="noreferrer noopener" className="user">GitHub</a>
+      </div>
     </header>
   );
 }
