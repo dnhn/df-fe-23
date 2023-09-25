329fe0b0a1890c7b02a35323d50f89e4e43ec586
Nhan Doan <2561973+dnhn@users.noreply.github.com>
2023-09-25 22:18:38 +0700

    Unit test

--- assignment-2/src/components/Button.js
+++ assignment-2/src/components/Button.js
@@ -1,8 +1,14 @@
-export default function Button({ children, type = 'button', variant, ...other}) {
+export default function Button({
+  children,
+  type = 'button',
+  variant,
+  classes = '',
+  ...other
+}) {
   return (
     <button
       type={type}
-      className={`btn ${variant ? `btn--${variant}` : ''} ${other.className}`}
+      className={`btn ${variant ? `btn--${variant}` : ''} ${classes}`}
       {...other}
     >
       {children}
--- assignment-2/src/components/Button.test.js
+++ assignment-2/src/components/Button.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
+
 import Button from './Button';
 
 test('renders Button', () => {
-  render(<Button label="click me" />);
-  const element = screen.getByText(/click me/i);
-  expect(element).toBeInTheDocument();
+  render(<Button>click me</Button>);
+  expect(screen.getByText(/click me/i)).toBeInTheDocument();
 });
