6713c3edeaa9d685c47701672e53db3dcc54bed0
Nhan Doan <2561973+dnhn@users.noreply.github.com>
2023-09-26 19:33:15 +0700

    Theme switch

---
+++ assignment-3/src/components/theme-switch/ThemeSwitch.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react'
+
+import { getLocalStorageItem, setLocalStorageItem } from '../../common/utils'
+
+import './ThemeSwitch.css'
+
+type IThemeMode = string | 'light' | 'dark'
+
+enum THEME_MODE {
+  LIGHT = 'light',
+  DARK = 'dark',
+}
+
+export function ThemeSwitch() {
+  const storedTheme = getLocalStorageItem('themeMode')
+  const [themeMode, setThemeMode] = useState<IThemeMode>(
+    storedTheme === THEME_MODE.DARK ? storedTheme : THEME_MODE.LIGHT,
+  )
+
+  useEffect(() => {
+    setLocalStorageItem('themeMode', themeMode)
+
+    if (themeMode === THEME_MODE.DARK) {
+      document.body.classList.add('dark-theme')
+    } else {
+      document.body.classList.remove('dark-theme')
+    }
+  }, [themeMode])
+
+  const toggleTheme = () =>
+    setThemeMode((mode: IThemeMode) =>
+      mode === THEME_MODE.DARK ? THEME_MODE.LIGHT : THEME_MODE.DARK,
+    )
+
+  return (
+    <button
+      type="button"
+      aria-label="Toggle theme"
+      onClick={toggleTheme}
+      className={`theme-switch ${
+        themeMode === 'dark' ? 'theme-switch--dark' : ''
+      }`}
+    >
+      <div className="light">☀️</div>
+      <div className="dark">🌙</div>
+    </button>
+  )
+}
