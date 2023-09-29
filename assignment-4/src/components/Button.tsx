592f32efa56bf57fe4a6aabefcd5ae1e4b9410a2
Nhan Doan <2561973+dnhn@users.noreply.github.com>
2023-09-29 08:55:03 +0700

    Update button

--- assignment-4/src/components/Button.tsx
+++ assignment-4/src/components/Button.tsx
@@ -2,21 +2,21 @@ import { ButtonHTMLAttributes } from 'react'
 
 interface IButton extends ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: 'error' | 'info' | 'primary' | 'warning'
-  classes?: string
 }
 
 export function Button({
   children,
   type = 'button',
   variant,
-  classes = '',
   ...other
 }: IButton) {
   return (
     <button
-      type={type}
-      className={`button ${variant ? `button--${variant}` : ''} ${classes}`}
       {...other}
+      type={type}
+      className={`button ${variant ? `button--${variant}` : ''} ${
+        other.className
+      }`}
     >
       {children}
     </button>
