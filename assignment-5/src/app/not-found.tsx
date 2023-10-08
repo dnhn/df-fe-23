d6b49376fb605435c5aeac1982c94abb5a91d5b4
Nhan Doan <2561973+dnhn@users.noreply.github.com>
2023-10-09 01:13:18 +0700

    Fix 404 layout

--- assignment-5/src/app/not-found.tsx
+++ assignment-5/src/app/not-found.tsx
@@ -1,5 +1,9 @@
 import NotFoundSection from '@/src/components/NotFoundSection'
 
 export default function NotFound() {
-  return <NotFoundSection />
+  return (
+    <main className="overflow-hidden px-4 pb-32 pt-16">
+      <NotFoundSection />
+    </main>
+  )
 }
