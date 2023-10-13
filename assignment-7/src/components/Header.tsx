350662594cf78d994e49f69cc72128ecf683b724
Nhan Doan <2561973+dnhn@users.noreply.github.com>
2023-10-13 20:57:39 +0700

    Replace HTML anchor tag with Next.js Link

--- assignment-7/src/components/Header.tsx
+++ assignment-7/src/components/Header.tsx
@@ -1,4 +1,5 @@
 import dynamic from 'next/dynamic'
+import Link from 'next/link'
 
 import { PATHS } from '@/src/lib/constants'
 import ThemeSwitch from '@/src/components/ThemeSwitch'
@@ -10,9 +11,9 @@ export default function Header() {
   return (
     <header className="bg-slate-100 px-4 py-3 shadow shadow-black/30 transition-colors ease-linear dark:bg-slate-700">
       <Container className="flex items-center justify-between">
-        <a href={PATHS.ROOT}>
+        <Link href={PATHS.ROOT}>
           <h1 className="text-xl font-bold sm:text-2xl">📚 Bookstore</h1>
-        </a>
+        </Link>
         <div className="flex items-center gap-4">
           <ThemeSwitch />
           <UserMenu />
