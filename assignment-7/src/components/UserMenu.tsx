0283fa3efd2062cbef48c771339baf894f40bcd2
Nhan Doan <2561973+dnhn@users.noreply.github.com>
2023-10-14 12:19:30 +0700

    Improve accessibility for user menu

--- assignment-7/src/components/UserMenu.tsx
+++ assignment-7/src/components/UserMenu.tsx
@@ -1,16 +1,26 @@
 'use client'
 
+import { useState } from 'react'
 import Image from 'next/image'
 
 import { useAuthContext } from '@/src/auth/AuthContext'
 
 export default function UserMenu() {
   const { auth, logout, user } = useAuthContext()
+  const [show, setShow] = useState(false)
+
+  const handleFocus = () => setShow(true)
+  const handleBlur = () => setShow(false)
+
+  const eventHandlers = {
+    onFocus: handleFocus,
+    onBlur: handleBlur,
+  }
 
   return (
     auth && (
       <div className="group relative">
-        <button className="flex items-center gap-2">
+        <button {...eventHandlers} className="peer flex items-center gap-2">
           {user && (
             <Image
               src={user.avatar}
@@ -24,7 +34,13 @@ export default function UserMenu() {
             {user ? user.fullName : 'User'}
           </span>
         </button>
-        <div className="pointer-events-none absolute right-0 top-[80%] z-[1] w-48 select-none overflow-hidden rounded bg-white text-sm opacity-0 shadow-md transition-[opacity,top] group-hover:pointer-events-auto group-hover:top-full group-hover:select-auto group-hover:opacity-100 dark:bg-slate-500">
+        <div
+          className={`absolute right-0 z-[1] w-48 overflow-hidden rounded bg-white text-sm shadow-md transition-[opacity,top] group-hover:pointer-events-auto group-hover:top-full group-hover:select-auto group-hover:opacity-100 peer-focus:pointer-events-auto peer-focus:top-full peer-focus:select-auto peer-focus:opacity-100 dark:bg-slate-500 ${
+            show
+              ? 'pointer-events-auto top-full select-auto opacity-100'
+              : 'pointer-events-none top-[80%] select-none opacity-0'
+          }`}
+        >
           {user && (
             <>
               <div className="px-3 py-2">
@@ -37,6 +53,8 @@ export default function UserMenu() {
           <button
             type="button"
             onClick={logout}
+            {...eventHandlers}
+            tabIndex={show ? 0 : -1}
             className="w-full px-3 py-2 text-left font-bold hover:bg-slate-100 dark:hover:bg-slate-400"
           >
             Log out
